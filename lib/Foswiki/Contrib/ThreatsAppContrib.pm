10ed07d6ad936ae3bbe5d12234efe3123e6d1eb2
Vincent Schänzer <schaenzer@modell-aachen.de>
2014-06-18 14:43:29 +0200

    initial commit
    Ticket:#4840

---
+++ lib/Foswiki/Contrib/ThreatsAppContrib.pm
@@ -0,0 +1,32 @@
+# See bottom of file for default license and copyright information
+package Foswiki::Contrib::ThreatsAppContrib;
+use strict;
+use warnings;
+
+our $VERSION = '1';
+our $RELEASE = "1";
+
+our $SHORTDESCRIPTION = 'Modell Aachen Threats WikiApp';
+
+1;
+
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Author: Modell Aachen GmbH 
+
+Copyright (C) 2008-2011 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
